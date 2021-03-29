2ee14c27c69c335312ad52414285902166a160b0
sgonz149@gmail.com <sgonz149@gmail.com>
2021-03-29 17:06:02 -0500

    created .net project and created startup script for project

---
+++ EmployeeManager/EmployeeManager/EmployeeManager/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using EmployeeManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EmployeeManager
+{
+    //stating this is where the application will startup
+    class Startup: BootstrapperBase
+    {
+        public Startup()
+        {
+            //configuration required for framework
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //for viewmodel, when app starts use mainviewmodel
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ EmployeeManager/EmployeeManager/EmployeeManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.ViewModels
+{
+    //inherit from screen
+    class MainViewModel : Screen
+    {
+    }
+}
