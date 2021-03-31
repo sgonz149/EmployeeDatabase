08f4b842d448466695da1132ad84e0448d10d222
sgonz149@gmail.com <sgonz149@gmail.com>
2021-03-31 13:25:03 -0500

    created employee and company models for app

---
+++ EmployeeManager/EmployeeManager/EmployeeManager/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    class EmployeeModel
+    {
+        public int EmployeeId {get; set;}
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public bool IsCommitted { get; set; }
+    }
+}
